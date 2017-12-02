6c53e5101220a3114739b02e47d8d9abfede0e9d
Corey Rocchio <crocchio85@gmail.com>
2017-12-02 16:53:53 -0500

    Moved Budget script to separate js file and included it in Scripts section after footer (_Layout.cshtml)

---
+++ MonthlyBudget/wwwroot/js/budget/budget.js
@@ -0,0 +1,10 @@
+﻿$(document).ready(function () {
+    function updateLink() {
+        var m = $('#BudgetMonth').val();
+        var y = $('#BudgetYear').val();
+        $('#changemonth').attr('href', '/Budget/Index?m=' + m + '&y=' + y);
+    }
+
+    $('#BudgetYear').change(updateLink);
+    $('#BudgetMonth').change(updateLink);
+});
