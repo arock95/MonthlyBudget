690e0a75a5775368b5e85eb74d62f6e36a9962f6
Anthony Rocchio <anthrock@hotmail.com>
2018-01-07 20:57:43 -0500

    prev and next links on budget and home controller

---
+++ MonthlyBudget/wwwroot/js/budget/prevNext.js
@@ -0,0 +1,26 @@
+﻿function prevNextLinks(mon, yr, cont) {
+    var prev = document.getElementById('previous');
+    var next = document.getElementById('next');
+
+    var prevm = mon;
+    var nextm = mon;
+    var prevy = yr;
+    var nexty = yr;
+
+    if (mon == 1) {
+        prevm = 12;
+        nextm++;
+        prevy--;
+    }
+    else if (mon == 12) {
+        nextm = 1;
+        nexty++;
+        prevm--;
+    }
+    else {
+        prevm--;
+        nextm++;
+    }
+    prev.setAttribute('href', '/' + cont + '/Index?m=' + prevm + '&y=' + prevy);
+    next.setAttribute('href', '/' + cont + '/Index?m=' + nextm + '&y=' + nexty);
+}
